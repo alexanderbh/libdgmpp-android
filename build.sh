07a09398dfc467aff458a38002cc0daa1fddf89a
Alexander <alexanderbh@gmail.com>
2016-10-15 18:01:36 +0200

    update

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ echo 'Running swig...'
 swig -c++ -java -package dgmpp -outdir java/dgmpp -o java/jni/dgmpp_wrap.cxx libdgmpp/swig/dgmpp.i
 cp libdgmpp/dgmpp/*.cpp java/jni/
 cp libdgmpp/dgmpp/*.h java/jni/
+
 cp libdgmpp/dgmpp/ThirdParty/*.* java/jni/ThirdParty
 cp Android.mk java/jni/Android.mk
 cp Application.mk java/jni/Application.mk
